d39c599854497627b6cdabfc286bee449675487b
dinesh katwal <dinesh@auzmor.com>
2020-01-29 14:56:37 +0530

    Added run.sql and drain.sql

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/k2glyph/latency-check
 go 1.12
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.6.2
 )
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"strings"
+	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/k2glyph/latency-check/commands/mysql"
 )
 
 var (
@@ -13,6 +18,54 @@ var (
 	cfgFile string
 )
 
+func dbConn() (db *sql.DB) {
+	dbUser := viper.GetString("mysql.username")
+	dbPass := viper.GetString("mysql.password")
+	dbName := viper.GetString("mysql.database")
+	dbHost := viper.GetString("mysql.hostname")
+	dbPort := viper.GetString("mysql.port")
+	db, err := sql.Open("mysql", dbUser+":"+dbPass+"@tcp("+dbHost+":"+dbPort+")/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+func dbExec(file []byte) {
+	db := dbConn() //open connection
+	requests := strings.Split(string(file), ";")
+	requests = requests[:len(requests)-1]
+	for _, request := range requests {
+		_, err := db.Exec(request)
+		if err != nil {
+			panic(err)
+		}
+	}
+	defer db.Close()
+}
+func mysqlCommand() *cobra.Command {
+	var mysqlLatency = &cobra.Command{
+		Use:   "mysql-check",
+		Short: "Check latency of mysql remote server",
+		Long:  `Check latency of mysql remote server with running sql file`,
+		Run: func(cmd *cobra.Command, args []string) {
+			startTime := time.Now()
+			if file, err := ioutil.ReadFile(viper.GetString("mysql.runsqlfile")); err != nil {
+				log.Fatal("No run sql file found")
+			} else {
+				dbExec(file)
+			}
+			if file, err := ioutil.ReadFile(viper.GetString("mysql.drainsqlfile")); err != nil {
+				log.Fatal("No drain sql file found")
+			} else {
+				dbExec(file)
+			}
+			endTime := time.Now()
+			fmt.Println("Total time took:", endTime.Sub(startTime))
+		},
+	}
+	// mysqlLatency.Flags().StringVar(&hostname, "hostname", "", "hostname of remote mysql server")
+	return mysqlLatency
+}
 func main() {
 
 	var rootCmd = &cobra.Command{
